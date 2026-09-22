da3a101d7937a1e1c2dde11a5877518eee37b0c9
raj-khan <meherullah97@gmail.com>
2026-09-22 21:03:20 +0800

    FL-21: Pan the canvas to a requested node

--- src/stores/canvas.js
+++ src/stores/canvas.js
@@ -6,10 +6,22 @@ export const useCanvasStore = defineStore('canvas', () => {
   /** @type {import('vue').Ref<{ x: number, y: number, zoom: number } | null>} */
   const viewport = ref(null)
 
+  /** Node the canvas should pan to next, such as one just created. */
+  const focusNodeId = ref('')
+
   /** @param {{ x: number, y: number, zoom: number }} next */
   function setViewport(next) {
     viewport.value = next
   }
 
-  return { viewport, setViewport }
+  /** @param {string} id */
+  function requestFocus(id) {
+    focusNodeId.value = id
+  }
+
+  function clearFocus() {
+    focusNodeId.value = ''
+  }
+
+  return { viewport, focusNodeId, setViewport, requestFocus, clearFocus }
 })
