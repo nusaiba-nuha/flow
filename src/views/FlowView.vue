2cb018d290f8eee8393700181c67a6d3876fd6e9
raj-khan <meherullah97@gmail.com>
2026-09-22 20:34:28 +0800

    FL-14: Render the flow canvas
    Nodes go over with setNodes; fitView waits for nodes-initialized.

--- src/views/FlowView.vue
+++ src/views/FlowView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+
+import FlowCanvas from '@/components/canvas/FlowCanvas.vue'
 </script>
 
 <template>
   <div class="flex h-full w-full flex-col">
-    <p class="p-6 text-sm">Canvas</p>
+    <FlowCanvas />
 
     <!-- Nested, so opening a node never unmounts the canvas. -->
     <RouterView />
